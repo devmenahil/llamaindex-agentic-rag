631ab5a6a87f8ee2bef6db1564efe0e2df8c7ea4
Menahil <menahil.shamas@gmail.com>
2026-08-19 12:30:02 +0400

    Update utils.py

---
+++ router-query-engine/utils.py
@@ -0,0 +1,22 @@
+import os
+
+from helper import get_google_api_key
+from utils import get_router_query_engine
+
+os.environ["GOOGLE_API_KEY"] = get_google_api_key()
+
+query_engine = get_router_query_engine("evaluation-report.pdf")
+
+response = query_engine.query(
+    "What is the summary of the document?"
+)
+
+print(response)
+
+print("\n" + "-" * 80 + "\n")
+
+response = query_engine.query(
+    "How do agents share information with other agents?"
+)
+
+print(response)
